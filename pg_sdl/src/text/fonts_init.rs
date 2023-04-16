9c3205e80108837a078775ee692a9ce087372f96
Jungo Arnaud <arnolo.jo@gmail.com>
2023-04-16 18:44:52 +0200

    coucou

--- pg_sdl/src/text/fonts_init.rs
+++ pg_sdl/src/text/fonts_init.rs
@@ -2,6 +2,16 @@
 Returns a vector of fonts.
 The fonts are loaded from the system at compile time (using the `include_bytes!` macro).
 */
+#[cfg(windows)]
+pub fn fonts_init() -> Vec<fontdue::Font> {
+    macros::init_fonts!(
+        "C:/Users/arnol/PycharmProjects/LibTests/venv/Lib/site-packages/kivy/data/fonts",
+        "DejaVuSans.ttf",
+        "DejaVuSans.ttf",
+    )
+}
+
+#[cfg(linux)]
 pub fn fonts_init() -> Vec<fontdue::Font> {
     macros::init_fonts!(
         "/usr/share/fonts/TTF",
