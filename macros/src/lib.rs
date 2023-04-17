f35c45b7560ea357fc3b61bce7c093c3b59704fe
Quentin Jungo <quentin.jungo@gmail.com>
2023-04-17 17:13:24 +0200

    Update lib.rs

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -6,24 +6,21 @@ pub fn init_fonts(input: TokenStream) -> TokenStream {
     let input = input.to_string();
     let mut tokens = input.split(",").collect::<Vec<&str>>();
     let folder = tokens.remove(0).trim().trim_matches('"');
-
-    let files = &input[input.find('[').unwrap() + 1..input.find(']').unwrap()];
-    let files = files.split(',').collect::<Vec<&str>>();
-
+    let files = input[input.find('[').unwrap() + 1..input.find(']').unwrap()]
+        .split(',')
+        .collect::<Vec<&str>>();
     let mut result = "(vec![".to_string();
     for font in &files {
         if font.trim().trim_matches('"').is_empty() {
             continue;
         }
         let path = format!("{}/{}", folder, font.trim().trim_matches('"'));
-        // let path = format!("{}/{}", folder, font.);
         result.push_str(&quote! {
             fontdue::Font::from_bytes(include_bytes!(#path) as &[u8], Default::default()).unwrap(),
         }
         .to_string());
     }
     result.push_str("], HashMap::from([");
-
     for (i, font) in files.iter().enumerate() {
         if font.trim().trim_matches('"').is_empty() {
             continue;
@@ -31,7 +28,6 @@ pub fn init_fonts(input: TokenStream) -> TokenStream {
         let path = format!("({}.to_string(),{}),", font.trim(), i);
         result.push_str(&path);
     }
-
     result.push_str("]))");
     result.parse().unwrap()
 }
