68948ac016bacfd74dfdbd29add87e01ea00fbf8
Quentin Jungo <quentin.jungo@gmail.com>
2023-05-02 11:31:20 +0200

    update lib system

--- pg_sdl/src/text/mod.rs
+++ pg_sdl/src/text/mod.rs
@@ -1,5 +1,3 @@
-mod fonts_init;
-mod q_draw_text;
 use std::collections::HashMap;
 mod text;
 
@@ -14,25 +12,20 @@ pub use text::Text;
 
 pub struct TextDrawer {
     pub texture_creator: sdl2::render::TextureCreator<sdl2::video::WindowContext>,
-    pub fonts: Vec<fontdue::Font>,
-    pub fonts_id: HashMap<String, usize>,
 }
 
 impl TextDrawer {
     pub fn new(texture_creator: sdl2::render::TextureCreator<sdl2::video::WindowContext>) -> Self {
-        let (fonts, fonts_id) = fonts_init::fonts_init();
-        println!("{:?}", fonts_id);
+
         TextDrawer {
             texture_creator,
-            fonts,
-            fonts_id,
-        }
+                }
     }
 
-    // TODO use this to call the [draw_text] fn with a font name instead of its index
-    fn font_index_from_name(&self, name: &str) -> Option<&usize> {
-        self.fonts_id.get(name)
-    }
+    // // TODO use this to call the [draw_text] fn with a font name instead of its index
+    // fn font_index_from_name(&self, name: &str) -> Option<&usize> {
+    //     self.fonts_id.get(name)
+    // }
     
     pub fn draw(
         &mut self,
@@ -44,31 +37,31 @@ impl TextDrawer {
         horizontal_align: HorizontalAlign,
         vertical_align: VerticalAlign,
     ) {
-        let mut font_texture = FontTexture::new(&self.texture_creator).unwrap();
-        let mut layout = Layout::new(CoordinateSystem::PositiveYDown);
+        // let mut font_texture = FontTexture::new(&self.texture_creator).unwrap();
+        // let mut layout = Layout::new(CoordinateSystem::PositiveYDown);
         
-        let width = Some(if width == None{
-            text.text.len() as f32 * text.font_size
-        } else { width.unwrap() });
-        layout.reset(&LayoutSettings {
-            x: position.x as f32,
-            y: position.y as f32,
-            max_width: width,
-            max_height: height,
-            horizontal_align,
-            vertical_align,
-            wrap_style: fontdue::layout::WrapStyle::Word,
-            wrap_hard_breaks: false,
-            line_height: 1.,
-        });
-        layout.append(
-            &self.fonts,
-            &TextStyle::with_user_data(
-                &text.text,
-                text.font_size,
-                text.font_index,
-                text.color),
-        );
-        font_texture.draw_text(canvas, &self.fonts, layout.glyphs()).unwrap();
+        // let width = Some(if width == None{
+        //     text.text.len() as f32 * text.font_size
+        // } else { width.unwrap() });
+        // layout.reset(&LayoutSettings {
+        //     x: position.x as f32,
+        //     y: position.y as f32,
+        //     max_width: width,
+        //     max_height: height,
+        //     horizontal_align,
+        //     vertical_align,
+        //     wrap_style: fontdue::layout::WrapStyle::Word,
+        //     wrap_hard_breaks: false,
+        //     line_height: 1.,
+        // });
+        // layout.append(
+        //     &self.fonts,
+        //     &TextStyle::with_user_data(
+        //         &text.text,
+        //         text.font_size,
+        //         text.font_index,
+        //         text.color),
+        // );
+        // font_texture.draw_text(canvas, &self.fonts, layout.glyphs()).unwrap();
     }
 }
