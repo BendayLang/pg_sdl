5e285d78650759fb4ce7cba9ed605e20672b3546
Quentin Jungo <quentin.jungo@gmail.com>
2023-05-08 04:22:57 +0200

    Update basic.rs

--- examples/basic.rs
+++ examples/basic.rs
@@ -2,11 +2,14 @@ use pg_sdl::prelude::*;
 use pg_sdl::widgets::text_input::TextInput;
 use pg_sdl::widgets::Widgets;
 
+// Here we define our app-state struct
 pub struct MyApp {
     pub draw_circle: bool,
 }
 
+// To call the run function of PgSdl, we need to implement the App trait for our app-state struct
 impl App for MyApp {
+    // The update function is called every frame, and is used to update the app-state
     fn update(&mut self, _delta: f32, _input: &Input, widgets: &mut Widgets) -> bool {
         let mut changed = false;
         if self.draw_circle {
@@ -20,7 +23,10 @@ impl App for MyApp {
         changed
     }
 
+    // The draw function is called every frame, if update returned true or any widget has changed
+    // It is called just after the update function
     fn draw(&mut self, canvas: &mut Canvas<Window>, _text_drawer: &mut TextDrawer) {
+        // We can put any custom drawing code here
         if self.draw_circle {
             canvas.set_draw_color(Colors::VIOLET);
             draw_circle(canvas, point!(500, 400), 100, 20);
@@ -40,12 +46,19 @@ impl App for MyApp {
                 .collect::<Vec<Rect>>();
             canvas.draw_rects(&rects).unwrap();
         }
+        // All the widgets are drawn automatically by PgSdl
     }
 }
 
 fn main() {
+    // First we initialize our custom app-state struct
     let mut my_app = MyApp { draw_circle: false };
+
+    // Then we initialize the PgSdl struct
     let mut pd_sdl: PgSdl = PgSdl::init("Benday", 1200, 720, Some(60), true, Colors::SKY_BLUE);
+
+    // We can add widgets to the PgSdl struct (as long as they implement the Widget trait)
+    // We will retrieve them later in the update function with the name we gave them
     pd_sdl
         .add_widget(
             "button",
@@ -77,5 +90,7 @@ fn main() {
                 Some(Text::new("Auto !".to_string(), 16, None)),
             )),
         );
+
+    // Finally we run the app, that take a mutable reference to our custom app-state struct
     pd_sdl.run(&mut my_app);
 }
