8c5d1498eaa7c78665783543885f9772e8e11015
Quentin Jungo <quentin.jungo@gmail.com>
2023-07-08 15:06:27 +0200

    caca print & while

--- examples/benday/blocs/while.rs
+++ examples/benday/blocs/while.rs
@@ -10,11 +10,13 @@ use std::collections::HashMap;
 
 use super::containers::{HoveredOn, Slot};
 
-pub struct Print {
+pub struct While {
 	skeleton: Skeleton,
 }
-impl Print {
+
+impl While {
 	const COLOR: Color = hsv_color(330, 0.3, 1.0);
+	const TEXT_WIDTH: f64 = 0.0;
 
 	fn new(position: Point2<f64>, blocs: &HashMap<u32, Box<dyn Bloc>>) -> Self {
 		let mut bloc = Self {
@@ -32,24 +34,28 @@ impl Print {
 	}
 }
 
-impl Bloc for Print {
+impl Bloc for While {
 	fn get_size(&self, blocs: &HashMap<u32, Box<dyn Bloc>>) -> Vector2<f64> {
-		self.skeleton.slots.get(0).unwrap().get_size(blocs) + Vector2::new(2.0, 2.0) * Skeleton::MARGIN
+		// width = self.sequences[0].size.x + self.slots[0].size.x + self.text_width
+		// height = max(self.slots[0].size.y, self.sequences[0].size.y)
+		// return Vec2(width, height) + Vec2(2 * MARGIN) + Vec2(3, 0) * INNER_MARGIN
+
+		let width =
+			self.skeleton.sequences[0].get_size(blocs).x + self.skeleton.slots[0].get_size(blocs).x + Self::TEXT_WIDTH;
+		let height = self.skeleton.slots[0].get_size(blocs).y.max(self.skeleton.sequences[0].get_size(blocs).y);
+		let inner_margin: f64 = todo!("inner margin");
+		Vector2::new(width, height) + Vector2::new(2.0, 2.0) * Skeleton::MARGIN + Vector2::new(3.0, 0.0) * inner_margin;
 	}
 
 	fn slot_position(&self, slot_id: u16) -> Vector2<f64> {
-		// python: `return Vec2(TEXT_PRINT_SIZE.x + INNER_MARGIN, 0) + Vec2(MARGIN)
-		let inner_margin: f64 = todo!("inner margin");
-		Vector2::new(self.skeleton.size.x + inner_margin, 0.0) + Vector2::new(2.0, 2.0) * Skeleton::MARGIN
+		// python: `return Vec2(TEXT_While_SIZE.x + INNER_MARGIN, 0) + Vec2(MARGIN)
+		let inner_margin = todo!("inner margin");
+		// Vector2::new(self.skeleton.size.x + inner_margin, 0.0) + Vector2::new(2.0, 2.0) * Skeleton::MARGIN
 	}
 
 	fn sequence_position(&self, sequence_id: u16) -> Vector2<f64> {
-		// position_x = self.slots[0].size.x + self.text_width + 3 * INNER_MARGIN
-		// position_y = sum([sequence.size.y for sequence in self.sequences[:sequence_id]]) +\
-		//              sequence_id * INNER_MARGIN
-		// return Vec2(position_x, position_y) + Vec2(1, 1) * MARGIN	}
-		let inner_margin: f64 = todo!("inner margin");
-		let mut position_x = self.skeleton.slots[0].get_size(&HashMap::new()).x + self.skeleton.size.x + 3.0 * inner_margin;
+		todo!("With a state pattern, this should not be callable (at compile time)");
+	}
 
 	fn button_size(&self, button_id: u16) -> Vector2<f64> {
 		todo!("With a state pattern, this should not be callable (at compile time)");
